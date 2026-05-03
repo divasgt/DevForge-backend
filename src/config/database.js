90e7509dca261c2ce3e603904247cb70b0291b7f
Divas <divas.verma1956@gmail.com>
2026-05-03 23:31:44 +0530

    Move MongoDB URI to environment variable for security

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "process.env.MONGODB_URI",
-  );
+  await mongoose.connect(process.env.MONGODB_URI);
 };
 
 module.exports = connectDB;
