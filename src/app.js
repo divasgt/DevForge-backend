74a2f8c9c540a8eaa67838197a28938e598031ea
Divas <divas.verma1956@gmail.com>
2026-05-06 16:00:39 +0530

    write get /findById, delete /user, patch /user apis

--- src/app.js
+++ src/app.js
@@ -34,6 +34,51 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+// using findById
+app.get("/findById", async (req, res) => {
+  const userId = req.body.id;
+  try {
+    const user = await User.findById(userId);
+    res.send(user);
+  } catch (err) {
+    res.send(400).send("Something went wrong!");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.id;
+  try {
+    const result = await User.findByIdAndDelete(userId);
+    console.log(result);
+    if (result) {
+      // && result instanceof User
+      res.send("User deleted sucessfully!");
+    } else {
+      res.status(404).send("User not found!");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+// Update user with email id
+app.patch("/user", async (req, res) => {
+  const { email: userEmail, newData } = req.body; // this is destructuring, using email field as userEmail variable
+  console.log(userEmail, newData);
+  try {
+    const result = await User.updateOne({ email: userEmail }, { ...newData });
+    console.log(result);
+    if (result.matchedCount === 1 && result.upsertedCount === 1) {
+      res.send("User data updated successfully!");
+    } else {
+      res.status(400).send("User not found!");
+    }
+  } catch (err) {
+    res.send(400).send("Something went wrong!");
+  }
+});
+
+// Create new user
 app.post("/signup", async (req, res) => {
   // creating a new instance of the User model
   const user1 = new User(req.body);
