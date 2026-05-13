d77958d57ac6f97381f579a528b46ea87796645b
Divas <divas.verma1956@gmail.com>
2026-05-13 14:22:29 +0530

    used jwt

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
-// Middleware to read or accept json from requests. Without writing this, if we console.log(req.body) we will get undefined.
-app.use(express.json());
+app.use(express.json()); // Middleware to read or accept json from requests. Without writing this, if we console.log(req.body) we will get undefined.
+app.use(cookieParser()); // Middleware to read cookies, without writing this, if we console.log(req.cookies) we get undefined.
 
 // GET /user api, get user by email
 app.get("/user", async (req, res) => {
@@ -52,7 +54,7 @@ app.delete("/user", async (req, res) => {
   const userId = req.body.id;
   try {
     const result = await User.findByIdAndDelete(userId);
-    console.log(result);
+    // console.log(result);
     if (result) {
       // && result instanceof User
       res.send("User deleted sucessfully!");
@@ -95,14 +97,14 @@ app.patch("/user/:userId", async (req, res) => {
 // Update user with email id, here when invalid gender is passed, idk why error is not being handled
 app.patch("/user", async (req, res) => {
   const { email: userEmail, newData } = req.body; // this is destructuring, using email field as userEmail variable
-  console.log(userEmail, newData);
+  // console.log(userEmail, newData);
   try {
     const result = await User.updateOne(
       { email: userEmail },
       { ...newData },
       { runValidators: true },
     );
-    console.log(result);
+    // console.log(result);
     if (result.matchedCount === 1 && result.modifiedCount === 1) {
       res.send("User data updated successfully!");
     }
@@ -111,23 +113,52 @@ app.patch("/user", async (req, res) => {
   }
 });
 
+app.get("/profile", async (req, res) => {
+  try {
+    const cookies = req.cookies;
+    const { token } = cookies;
+    if (!token) {
+      throw new Error("Invalid token.");
+    }
+
+    // Verify jwt token from cookie, and find user
+    const decodedMessage = await jwt.verify(token, process.env.JWT_SECRET);
+    const { _id } = decodedMessage;
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User does not exist.");
+    }
+
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
 app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
     if (!email || !password) {
       throw new Error("Email and password are required.");
     }
 
+    // find user from DB
+    const user = await User.findOne({ email });
     if (!user) {
       throw new Error("Invalid email id or password.");
     }
 
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
-    if (!isPasswordCorrect) {
-      throw new Error("Invalid email id or password");
-    } else {
+    // Validate password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      // Create a JWT token
+      const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790");
+
+      // Add token to cookie and send to user
+      res.cookie("token", token);
       res.send("Login successful!");
+    } else {
+      throw new Error("Invalid email id or password.");
     }
   } catch (err) {
     res.status(400).send("Error: " + err.message);
